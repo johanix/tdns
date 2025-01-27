d4bdc6f74071938fbb17ff96e308c048b0b180e7
Johan Stenstam <johani@johani.org>
2025-01-27 21:46:38 +0100

    foo

--- utils/make-version.sh
+++ utils/make-version.sh
@@ -3,17 +3,23 @@ appversion=$1
 appdate=$2
 prog=$3
 
-if [ "$prog" = "tdns-agent" ] || [ "$prog" = "tdns-server" ] || [ "$prog" = "music-sidecar" ]; then 
-	echo generating version.go
-	{
+versioned_progs="tdns-agent tdns-server music-sidecar"
+
+# if [ "$prog" = "tdns-agent" ] || [ "$prog" = "tdns-server" ] || [ "$prog" = "music-sidecar" ]; then 
+case " $versioned_progs " in
+	*" $prog "*)
+		echo generating version.go
+		{
 		echo "package main"
 		echo "const appVersion = \"${appversion}\""
 		echo "const appDate = \"${appdate}\""
 		echo "const appName = \"${prog}\""
-	} > version.go || {
+		} > version.go || {
 		echo "Error: Failed to generate version.go" >&2
 		exit 1
-	}
-else
-	echo not generating version.go
-fi
+		}
+		;;
+	*)
+		echo not generating version.go, $prog is not a versioned program
+		;;
+esac
