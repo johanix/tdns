335518248f81eac0bfbeb2b9db81350474405a02
Johan Stenstam <johani@johani.org>
2025-02-26 15:17:38 +0100

    foo

--- tdns/apirouters.go
+++ tdns/apirouters.go
@@ -71,9 +71,6 @@ func SetupCombinerAPIRouter(conf *Config) (*mux.Router, error) {
 	sr.HandleFunc("/command", APIcommand(conf)).Methods("POST")
 	sr.HandleFunc("/config", APIconfig(conf)).Methods("POST")
 	sr.HandleFunc("/zone", APIzone(&Globals.App, conf.Internal.RefreshZoneCh, kdb)).Methods("POST")
-	// XXX: this is a temporary endpoint that should migrate into the combiner
-	// endpoint.
-	sr.HandleFunc("/replace", APIzoneReplace(&Globals.App, conf.Internal.RefreshZoneCh, kdb)).Methods("POST")
 	sr.HandleFunc("/combiner", APICombiner(&Globals.App, conf.Internal.RefreshZoneCh, kdb)).Methods("POST")
 	sr.HandleFunc("/debug", APIdebug()).Methods("POST")
 	// sr.HandleFunc("/show/api", tdns.APIshowAPI(r)).Methods("GET")
